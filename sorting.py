22c17633bac09bcfe6c23e28fbe94a7dc09816da
Stephen R. Broughton <stephen.broughton@inin.com>
2016-04-26 19:45:24 -0400

    no message

---
+++ sorting.py
@@ -0,0 +1,71 @@
+#sorts array by userid or businessid
+def mergeSort(alist, ids):
+    if len(alist) > 1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+        
+        mergeSort(lefthalf, ids)
+        mergeSort(righthalf, ids)
+
+        i=0
+        j=0
+        k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if (ids == 'business_id'): #by business_id
+              if (lefthalf[i].business_id < righthalf[j].business_id):
+                  alist[k]=lefthalf[i]
+                  i=i+1
+              else:
+                  alist[k]=righthalf[j]
+                  j=j+1
+            else: #by user_id
+                if (lefthalf[i].user_id < righthalf[j].user_id):
+                  alist[k]=lefthalf[i]
+                  i=i+1
+                else:
+                  alist[k]=righthalf[j]
+                  j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+  
+
+#binary search by userid (must be sorted by userid)
+def binary_search(value, alist, ids):
+    last = len(alist) - 1
+    first = 0
+    found = None
+    
+
+    if (ids == 'user_id'): #by user_id
+        while first <= last and not found:
+           midpoint = (first + last) // 2
+           if (alist[midpoint].user_id == value):
+               found = True
+           else:
+                if (value < alist[midpoint].user_id):
+                    last = midpoint - 1
+                else:
+                    first = midpoint + 1
+        return found
+
+    else: #by business_id
+        while first <= last and not found:
+            midpoint = (first + last) // 2
+            if alist[midpoint].business_id == value:
+               found = True
+            else:
+                if value < alist[midpoint].business_id:
+                    last = midpoint - 1
+                else:
+                    first = midpoint+1
+        return found
